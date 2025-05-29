ea121e427098736f95b24f32bc8077aa40cb4c43
AlexanderSafiulin <115018603+greensaf@users.noreply.github.com>
2025-05-29 09:46:20 +0700

    w
    w

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-      './lib/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
   ],
-  safelist: ['grid-cols-[2fr_2fr_1fr]'],
+  safelist: [
+    'grid',
+    'grid-cols-3',
+    'grid-cols-1',
+    'sm:grid-cols-[2fr_2fr_1fr]',
+    'gap-8',
+  ],
   theme: {
-    extend: {
-      fontFamily: {
-        heading: ['"Playfair Display"', 'serif'],
-        body: ['"Inter"', 'sans-serif'],
-      },
+    extend: {},
       colors: {
         primary: '#536C4A', // основной зелёный
         accent: '#B0BF93', // светло-зелёный
