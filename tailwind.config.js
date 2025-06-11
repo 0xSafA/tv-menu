87fee37e59a9f77dd904a4f5497bec722016a04c
Alex <115018603+greensaf@users.noreply.github.com>
2025-06-11 16:44:48 +0700

    dd

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,35 +12,29 @@ module.exports = {
   ],
   theme: {
     extend: {
-      /* ─── цвета, фоны ─── */
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic' :
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic' : 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         primary: '#536C4A',
         accent : '#B0BF93',
       },
-
-      /* ─── анимации ─── */
       keyframes: {
-        /* Pac-Man рот */
-       pacChomp: {'0%,100%': {
-      'clip-path': 'polygon(50% 50%,100% 49%,100% 51%)',
-    },
-    '50%': {
-      'clip-path': 'polygon(50% 50%,100% 0,100% 100%)',
-      /* при желании можно добавить небольшую паузу:
-         animation-timing-function: steps(1); */
-    },
+        pacChomp: {
+          '0%,100%': {
+            'clip-path': 'polygon(50% 50%,100% 49%,100% 51%)',
+          },
+          '50%': {
+            'clip-path': 'polygon(50% 50%,100% 0,100% 100%)',
+          },
+        },
         pacPath: {
           '0%'  : { transform: 'translate(-40px,0)' },
           '60%' : { transform: 'translate(calc(1100px - 40px),0)' },
           '65%' : { transform: 'translate(calc(1100px - 40px),0) rotate(90deg)' },
           '100%': { transform: 'translate(calc(1100px - 40px),-900px) rotate(-90deg)' },
         },
-        /* дыхание точек */
         breath: {
           '0%,100%': { transform: 'scale(1)',  opacity: .8 },
           '50%'    : { transform: 'scale(1.35)', opacity: 1 },
@@ -51,12 +45,12 @@ module.exports = {
         },
       },
       animation: {
-        pacChomp : 'pacChomp .3s linear infinite',
-        pacPath  : 'pacPath 20s ease-in-out infinite',
-        breath   : 'breath 1.8s ease-in-out infinite',
-        breathLeaf: 'breathLeaf 1.8s ease-in-out infinite',
+        pacChomp   : 'pacChomp .3s linear infinite',
+        pacPath    : 'pacPath 20s ease-in-out infinite',
+        breath     : 'breath 1.8s ease-in-out infinite',
+        breathLeaf : 'breathLeaf 1.8s ease-in-out infinite',
       },
     },
   },
   plugins: [],
-};
+}
